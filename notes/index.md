8e5e0977d27f40fa67c0ee49c3be347c958c22aa
Purbayan Chowdhury <pur.cho.99@gmail.com>
2024-08-03 16:48:45 +0530

    create 1 file and update 1 file

---
+++ notes/index.md
@@ -0,0 +1,3 @@
+# Links
+
+- [System Design Topics - Part 1.md]("https://shivishbrahma.github.io/NotePub/notes/System Design Topics - Part 1.md")
--- scripts/update_links.sh
+++ scripts/update_links.sh
@@ -3,17 +3,17 @@
 website="https://shivishbrahma.github.io/NotePub/"
 
 for dir in . */; do
-    if [ "$dir" == "." ] || [ ! -d "$dir" ]; then
+    if [ "$dir" == "." ] || [ "$dir" == ".git" ] || [ ! -d "$dir" ]; then
         continue
     fi
     cd "$dir"
     echo $(date +"%Y-%m-%d %H:%M:%S:") "Updating links in $dir"
 
     # Check if dir has .md
-    if [ $(ls -1 *.md | grep -v "^README.md$" | wc -l) -gt 0 ]; then
-        ls -1 *.md | grep -v "^README.md$" | { read name; echo "- [$name](\"$website$dir$name\")"; } >README.md
+    if [ $(ls -1 *.md | grep -v "^index.md$" | wc -l) -gt 0 ]; then
+        ls -1 *.md | grep -v "^index.md$" | { read name; echo "- [$name](\"$website$dir$name\")"; } >index.md
     fi
-    sed -i -e 's/^/# Links\n\n/' README.md
+    sed -i -e 's/^/# Links\n\n/' index.md
 
     echo $(date +"%Y-%m-%d %H:%M:%S:") "Updated links in $dir!"
 done
